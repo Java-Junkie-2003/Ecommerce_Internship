dc026b5eceb06a567b7ab15ed73b03a80944f742
Java-Junkie-2003 <21078081.hieu@student.iuh.edu.vn>
2025-09-04 22:07:54 +0700

    feat: Implement user management functionality for admin; add routes and controller methods to retrieve users - #36

--- Server/src/routes/User/index.js
+++ Server/src/routes/User/index.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const {authentication, authenticatorForRoleAdmin} = require('../../auth/authUtils')
+const asyncHandler = require('../../helpers/asyncHandler')
+const UserController = require('../../controllers/user.controller')
 
+router.use(authentication)
+router.use(authenticatorForRoleAdmin)
+
+router.get('',asyncHandler(UserController.getUserForAdmin))
+router.get('/all', asyncHandler(UserController.getAllUserForAdmin))
+
+
+module.exports = router
