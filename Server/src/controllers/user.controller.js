1950f7b4279185ca852dacce3bc120ebc75658e2
Java-Junkie-2003 <21078081.hieu@student.iuh.edu.vn>
2025-09-05 11:49:54 +0700

    feat: Enhance getAllUserForAdmin functionality; add pagination and sorting support - #36

--- Server/src/controllers/user.controller.js
+++ Server/src/controllers/user.controller.js
@@ -1,11 +1,13 @@
 const UserService = require('../services/user.service')
-const {getAllUserForAdmin} = require('../models/repositories/user.repo')
 const {SuccessResponse} = require('../core/success.response')
 class UserController {
     getAllUserForAdmin = async (req, res, next) => {
         new SuccessResponse({
             message: 'Get all user',
-            metadata: await UserService.getAllUserForAdmin()
+            metadata: await UserService.getAllUserForAdmin({
+                userId: req.User.userId,
+                page: req.query.page
+            })
         }).send(res)
     }
     getUserForAdmin = async (req, res, next) => {
--- Server/src/models/repositories/user.repo.js
+++ Server/src/models/repositories/user.repo.js
@@ -6,22 +6,49 @@ const findUserByPhoneNumber = async ({ phone, select }) => {
     return await userModel.findOne({ phone }).select(getSelectData(select)).lean()
 }
 
-const findUserByUserName = async ({username, select ={}}) => {
-    return await userModel.findOne({user_name: username})
-    .select(getSelectData(select))
-    .lean()
+const findUserByUserName = async ({ username, select = {} }) => {
+    return await userModel.findOne({ user_name: username })
+        .select(getSelectData(select))
+        .lean()
 }
 
-const findUserById = async ({userId, select = []}) => {
-    return await userModel.findById({_id: userId})
-    .select(getSelectData(select))
-    .lean();
+const findUserById = async ({ userId, select = [] }) => {
+    return await userModel.findById({ _id: userId })
+        .select(getSelectData(select))
+        .lean();
 }
 
-const getAllUserForAdmin = async () => {
-    return await userModel.find({})
-    .select(getUnSelectData(['isActive', 'password']))
-    .lean()
+const getAllUserForAdmin = async ({ userId, limit, page, sort }) => {
+    const sortBy = sort === 'ctime' ? { _id: 1 } : { _id: -1 }
+    const limitNum = Math.max(1, Number(limit) || 10)
+    const pageNum = Math.max(1, Number(page) || 1)
+    const skip = (pageNum - 1) * limitNum
+    const [results, total] = await Promise.all([
+        await userModel.find({ _id: { $ne: convertToObjectId(userId) } })
+            .sort(sortBy)
+            .skip(skip)
+            .limit(limitNum)
+            .select(getUnSelectData(['isActive', 'password']))
+            .lean()
+            .exec(),
+        userModel.countDocuments({ _id: { $ne: convertToObjectId(userId) } })
+    ])
+
+    const totalPages = Math.max(1, Math.ceil(total / limitNum))
+    const hasNext = pageNum < totalPages
+    const hasPrev = pageNum > 1
+
+     return {
+        results,
+        pagination: {
+            totalResult: total,
+            page: pageNum,
+            limit: limitNum,
+            totalPages,
+            hasNext,
+            hasPrev
+        }
+    }
 }
 
 
