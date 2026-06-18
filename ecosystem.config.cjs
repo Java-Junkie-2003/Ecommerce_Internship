2fd162380103a8ed815628d801cc460e5532610f
bdhieu03 <vnraizo2003@gmail.com>
2026-06-18 15:06:30 +0700

    adjust ecosystem config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -11,22 +11,10 @@ module.exports = {
       env: {
         NODE_ENV: "dev",
         DEV_APP_PORT: 3055,
+        DEV_DB_USER=vnraizo,
+        DEV_DB_PASSWORD=daMblOpBhbu1IsSF,
+        DEV_DB_NAME=EcommerceDB,
       },
-    },
-    {
-      name: "ecommerce-client-dev",
-      cwd: "./Client",
-      script: "node_modules/@react-router/serve/bin.js",
-      args: "./build/server/index.js",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "300M",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3001,
-        NEXT_PUBLIC_API_URL: "/api",
-      },
-    },
-  ],
+    }
+  ]
 };
